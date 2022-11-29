9fdd9be1b7a29b17e4f81f99212598c75b9611bf
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2022-11-29 12:04:01 +0100

    fixup! Start with nvim

---
+++ nvim/.config/nvim/lua/configs/general.lua
@@ -0,0 +1,28 @@
+--                                            ___      
+--                                           /\_ \     
+--   __      __    ___      __   _ __    __  \//\ \    
+-- /'_ `\  /'__`\/' _ `\  /'__`\/\`'__\/'__`\  \ \ \   
+--/\ \L\ \/\  __//\ \/\ \/\  __/\ \ \//\ \L\.\_ \_\ \_ 
+--\ \____ \ \____\ \_\ \_\ \____\\ \_\\ \__/.\_\/\____\
+-- \/___L\ \/____/\/_/\/_/\/____/ \/_/ \/__/\/_/\/____/
+--   /\____/                                           
+--   \_/__/                                            
+
+-- Visual settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.linebreak = true
+vim.opt.scrolloff = 5
+vim.opt.cursorline = true
+--vim.opt.signcolumn = 'yes'
+
+-- Searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+
+-- Indention
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
