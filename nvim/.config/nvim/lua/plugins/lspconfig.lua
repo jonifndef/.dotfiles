b298fc4f6acd4fd0f2db24ade7b39cd6915f412d
Jonas Fredriksson <jonas.fredriksson@combitech.com>
2023-01-03 10:18:41 +0100

    Add lsp mappings, TODO doc

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,5 +1,5 @@
 local servers = {
-  -- clangd = {},
+  clangd = {},
   -- gopls = {},
   pyright = {},
   -- rust_analyzer = {},
@@ -12,20 +12,38 @@ local servers = {
   },
 }
 
-require('mason').setup()
-local mason_lspconfig = require 'mason-lspconfig'
+require("mason").setup()
+local mason_lspconfig = require "mason-lspconfig"
 
 mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
 
+vim.keymap.set("n", "<Leader>dp", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "<Leader>dn", vim.diagnostic.goto_next)
+
+local on_attach = function(_, bufnr)
+  --vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references)
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+  vim.keymap.set("n", "<Leader>gD", vim.lsp.buf.type_definition)
+  vim.keymap.set("n", "<Leader>K", vim.lsp.buf.hover)
+  vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename)
+  vim.keymap.set("n", "<Leader>co", vim.lsp.buf.code_action)
+  vim.keymap.set("n", "<Leader>fo", vim.lsp.buf.format)
+  --vim.keymap.set("n", "<Leader>sd", vim.lsp.buf.signature_help)
+
+end
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 --local capabilities = vim.lsp.protocol.make_client_capabilities()
 --capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 mason_lspconfig.setup_handlers {
   function(server_name)
-    require('lspconfig')[server_name].setup {
+    require("lspconfig")[server_name].setup {
       --capabilities = capabilities,
       on_attach = on_attach,
       settings = servers[server_name],
