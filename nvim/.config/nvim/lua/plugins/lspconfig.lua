91ece4735e0f5797e120453e4e402f99ec6c393e
Jonas Fredriksson <jonas.fredriksson@combitech.com>
2023-04-05 12:58:28 +0200

    Try hover diagnostics

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -45,6 +45,37 @@ local on_attach = function(client, bufnr)
 
 end
 
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    float = {
+        border = "single",
+        format = function(diagnostic)
+            return string.format(
+                "%s (%s) [%s]",
+                diagnostic.message,
+                diagnostic.source,
+                diagnostic.code or diagnostic.user_data.lsp.code
+            )
+        end,
+    },
+})
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  pattern = "*",
+  group = vim.api.nvim_create_augroup("LSPDiagnosticsOnHover", {}),
+  callback = function()
+    local current_cursor = vim.api.nvim_win_get_cursor(0)
+    local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or { nil, nil }
+    -- Show the pop-up diagnostics window,
+    -- but only once for the current cursor location (unless moved afterwards).
+    if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then
+      vim.w.lsp_diagnostics_last_cursor = current_cursor
+      vim.diagnostic.open_float(0, { scope = "cursor" })
+    end
+  end
+})
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
