fe6bfc6b1c388c41f151462e7b6e2f49855d9fd7
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2025-11-25 22:34:02 +0100

    Working build-dev-container up until installing nix

--- scripts/.local/bin/scripts/build-dev-container.sh
+++ scripts/.local/bin/scripts/build-dev-container.sh
@@ -2,8 +2,24 @@
 
 set -euo pipefail
 
+cleanup()
+{
+    if [ -n "${OG_IMAGE_NAME}" ]; then
+        docker image rm "${OG_IMAGE_NAME}"
+        docker system prune
+    fi
+
+    [ -d "/tmp/.dotfiles" ] && rm -rf "/tmp/.dotfiles"
+    [ -d "/tmp/Busybox-static" ] && rm -rf "/tmp/Busybox-static"
+}
+
+trap 'cleanup' ERR
+
 DOCKERFILE_PATH=""
 OG_IMAGE_NAME=""
+NIX_TARBALL_URL="https://releases.nixos.org/nix/nix-2.32.4/nix-2.32.4-x86_64-linux.tar.xz"
+NIX_TARBALL=""
+DEV_USER="dev"
 
 usage()
 {
@@ -82,40 +98,71 @@ build_og_image()
     docker build -t "${OG_IMAGE_NAME}" -f "${DOCKERFILE_PATH}" .
 }
 
-cleanup()
-{
-    if [ -n "${OG_IMAGE_NAME}" ]; then
-        docker image rm "${OG_IMAGE_NAME}"
-        docker system prune
-    fi
-}
-
 set_username()
 {
     # One weakness here, what if there is a group with id 1000, without a user with id 1000?
     USERNAME=$(docker run --rm "${OG_IMAGE_NAME}" sh -c 'grep "1000:1000" /etc/passwd | cut -d: -f1')
 }
 
-build_overlay()
+download_dependencies_and_create_context()
 {
     CONTEXT=$(mktemp -d)
-    DEV_USER="dev"
+    git clone https://github.com/jonifndef/Busybox-static.git /tmp/Busybox-static
+    cp /tmp/Busybox-static/busybox_x86 "${CONTEXT}/busybox"
 
+    git clone --branch ubuntu-cab https://github.com/jonifndef/.dotfiles.git /tmp/.dotfiles
+    cp -r /tmp/.dotfiles "${CONTEXT}/.dotfiles"
+}
+
+build_overlay()
+{
     docker build -t devcontainer_jonas -f - "${CONTEXT}" <<-EOF
-        FROM $OG_IMAGE_NAME
+FROM $OG_IMAGE_NAME
 
-        RUN set -eux \
-            if [ -z $USERNAME ]; then \
-                if usermod -v > /dev/null 2>&1; then \
-                    # We are prolly in arch-/debian-/fedoraland \
-                    groupadd -g 1000 $DEV_USER; \
-                    useradd -m -u 1000 -g 1000 -s /bin/sh $DEV_USER; \
-                else \
-                    # We are prolly in alpine-with-just-busybox-land \
-                    addgroup -g 1000 $DEV_USER; \
-                    useradd -D -u 1000 -G $DEV_USER $DEV_USER; \
-                fi
+RUN set -eux; \
+if [ "${USERNAME}x" = "x" ]; then \
+if usermod -v > /dev/null 2>&1; then \
+groupadd -g 1000 $DEV_USER; \
+useradd -m -u 1000 -g 1000 -s /bin/sh $DEV_USER; \
+else \
+addgroup -g 1000 $DEV_USER; \
+adduser -D -u 1000 -G $DEV_USER $DEV_USER; \
+mkdir -p /home/$DEV_USER; \
+chown -R $DEV_USER:$DEV_USER /home/$DEV_USER; \
+fi; \
+fi
+
+RUN mkdir -p /nix && chmod 755 /nix
+
+COPY busybox /tmp/busybox
+COPY .dotfiles /tmp/.dotfiles
+RUN mv /tmp/.dotfiles /home/${USERNAME:-$DEV_USER}/
+RUN chmod 755 /tmp/busybox && \
+ln -s /tmp/busybox /tmp/tar && \
+ln -s /tmp/busybox /tmp/wget && \
+ln -s /tmp/busybox /tmp/xz && \
+ln -s /tmp/busybox /tmp/sha256sum && \
+chown -R ${USERNAME:-$DEV_USER}:${USERNAME:-$DEV_USER} /tmp && \
+chown -R ${USERNAME:-$DEV_USER}:${USERNAME:-$DEV_USER} /home/${USERNAME:-$DEV_USER}
+
+ENV USER=${USERNAME:-$DEV_USER}
+ENV HOME=/home/${USERNAME:-$DEV_USER}
+USER ${USERNAME:-$DEV_USER}
+WORKDIR /home/${USERNAME:-$DEV_USER}
+
+ENV PATH="\$PATH:/tmp"
+RUN mkdir -p .config && \
+ln -s "../.dotfiles/home-manager/.config/home-manager" ".config/home-manager" && \
+ln -s "../.dotfiles/nix/.config/nix" ".config/nix"
+
+
 EOF
+##RUN curl -L https://nixos.org/nix/install | sh
+#
+##ENV PATH="/home/${USERNAME:-$DEV_USER}/.nix-profile/bin:${PATH}"
+#
+##RUN nix run home-manager/master -- switch --flake .config/home-manager#ubuntu --impure
+#EOF
 }
 
 main()
@@ -124,6 +171,7 @@ main()
     set_dockerfile_path
     build_og_image
     set_username
+    download_dependencies_and_create_context
     build_overlay
     cleanup
 
