b6886e6f4cd4107801762eb025f0f7501b32bcfc
Jonas Fredriksson <jonas.fredriksson@combitech.com>
2026-06-29 08:13:38 +0200

    Add hyprmon, hyprlock, dunst

--- home-manager/.config/home-manager/desktop.nix
+++ home-manager/.config/home-manager/desktop.nix
@@ -40,6 +40,7 @@ in
     wiremix
     hyprlock
     networkmanagerapplet
+    dunst
   ];
 
   # The only applications that Home Mangager sets up are zsh with oh-my-zsh, the plugins, and fzf. Everything else is managed by standard dotfiles
